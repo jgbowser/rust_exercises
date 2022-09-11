d5ae942b02c21de5a3adf869631d4899ff466559
John <john.bow7720@gmail.com>
2022-09-10 23:09:50 -0600

    Chapter 9 Section 1: panic!

---
+++ chapter_9/src/main.rs
@@ -0,0 +1,6 @@
+// Chapter 9: Error Handling
+
+mod panicking;
+fn main() {
+    panicking::run();
+}
---
+++ chapter_9/src/panicking.rs
@@ -0,0 +1,12 @@
+// 9.1 Unrecoverable Errors with panic!
+
+pub fn run() {
+    // cause a panic
+    // panic!("crash and burn");
+
+    // Using a panic! Backtrace
+    // let v = vec![1, 2, 3];
+    // v[99];
+    // this panics, we can see the backtrace (think stack trace) by using the
+    // RUST_BACKTRACE env variable when using cargo run
+}
