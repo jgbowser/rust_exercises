650e846ec2c81a5a274d67355d4f01163d1b859e
John <john.bow7720@gmail.com>
2022-09-12 22:29:02 -0600

    Chapter 10 Section 1: Generics

--- chapter_10/src/generic_types.rs
+++ chapter_10/src/generic_types.rs
@@ -47,4 +47,80 @@ pub fn run() {
     println!("found the largest i32 using a generics func: {result2}");
     let result3 = largest(&char_list);
     println!("found the largest char using a generics func: {result3}");
+
+    // In Struct Definitions
+    // we can also define structs to use generics as well
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.1, y: 4.2 };
+    println!("x in the integer version is: {}", integer.x);
+    println!("y in the float version is: {}", float.y);
+    // we only defined Point with a single type parameter <T>, so all fields
+    // must be of the same type, we can specify multiple types though
+    #[derive(Debug)]
+    struct OtherPoint<T, U> {
+        _x: T,
+        _y: U,
+    }
+    // now these are all valid
+    let both_integer = OtherPoint { _x: 5, _y: 10 };
+    let both_float = OtherPoint { _x: 1.2, _y: 5.6 };
+    let integer_and_float = OtherPoint { _x: 5, _y: 8.9 };
+    println!(
+        "here are all the different forms now: {:?}, {:?}, {:?}",
+        both_integer, both_float, integer_and_float
+    );
+
+    // In Enum Definitions
+    // just like structs we can define enums with generic types
+
+    // Options are an example
+    enum _Option<T> {
+        Some(T),
+        None,
+    }
+
+    // In Method Definitions
+    impl<T> Point<T> {
+        fn x(&self) -> &T {
+            &self.x
+        }
+    }
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+    // we can also specify constraints on generic types when implementing methods
+    impl Point<f32> {
+        fn distance_from_origin(&self) -> f32 {
+            (self.x.powi(2) + self.y.powi(2)).sqrt()
+        }
+    }
+
+    let fp = Point { x: 4.0, y: 10.5 };
+
+    println!("distance from origin fp = {}", fp.distance_from_origin());
+    // we can implement methods with generics that don't match the struct definition
+    struct AnotherPoint<X1, Y1> {
+        x: X1,
+        y: Y1,
+    }
+
+    impl<X1, Y1> AnotherPoint<X1, Y1> {
+        fn mixup<X2, Y2>(self, other: AnotherPoint<X2, Y2>) -> AnotherPoint<X1, Y2> {
+            AnotherPoint {
+                x: self.x,
+                y: other.y,
+            }
+        }
+    }
+
+    let p1 = AnotherPoint { x: 5, y: 10.4 };
+    let p2 = AnotherPoint { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
